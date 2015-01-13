0aef2d02fe6b1a62ec8499b8359fc6e1bfc52010
serdyuk <serdyuk.dmitriy@gmail.com>
2015-01-13 10:20:25 -0500

    Added arguments and loading data

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -1,16 +1,31 @@
 __author__ = 'serdyuk'
 
 import numpy as np
+import cPickle as pkl
+import argparse
+import time
 
 
-def main():
-    data = None
-    labels = None
-    model = train(data, labels)
-    test(data, labels, model)
-    pass
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print '%s function took %0.3f ms' % (f.func_name, (time2-time1)*1000.0)
+        return ret
+    return wrap
 
 
+def main(filename, num_feat, num_layers, **kwargs):
+    train_data, valid_data, test_data = load_data(filename)
+
+    data, labels = train_data
+
+    model = train(data, labels, num_feat=num_feat, num_layers=num_layers)
+    print test(data, labels, model)
+
+
+@timing
 def train(data, labels, **kwargs):
     """
     Trains model
@@ -19,7 +34,7 @@ def train(data, labels, **kwargs):
     :return: Trained model, list of pairs (W, b) weight matrix W and bias b
              for each layer
     """
-    pass
+    return None
 
 
 def test(data, labels, model, **kwargs):
@@ -53,5 +68,26 @@ def backward_prop(data, labels, model):
     """
     pass
 
+
+def load_data(filename):
+    with open(filename, 'r') as fin:
+        datasets = pkl.load(fin)
+    return datasets
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Run MLP on MNIST')
+    parser.add_argument('filename',
+                        default='/data/lisa/data/mnist/mnist.pkl',
+                        help='File which contains pickled dataset')
+    parser.add_argument('num_feat', type=int,
+                        default=784,
+                        help='Number of input features')
+    parser.add_argument('num_layers', type=int,
+                        default=5,
+                        help='Number of layers of MLP')
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    main(**args.__dict__)
