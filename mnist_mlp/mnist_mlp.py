Fixed one issue with softmax gradient

mnist_mlp/mnist_mlp.py
@@ -21,8 +21,19 @@ def sigma(x):
     return 1. / (1. + np.exp(-x))
 
 
+def sigma_grad(y):
+    return y * (1. - y)
+
+
 def softmax(x):
-    return np.exp(x) / np.sum(np.exp(x), axis=0)
+    return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+
+
+def softmax_grad(y):
+    m, n = y.shape
+    delta = np.zeros((m, n, n))
+    delta[:, xrange(n), xrange(n)] = 1.
+    return y[:, :, None] * (delta - y[:, None, :])
 
 
 def pairwise(iterable):
@@ -86,13 +97,26 @@ def forward_prop(batch, model):
     hidden = batch
     values = []
     for W, b in model[:-1]:
-        value = W.dot(hidden.T) + b
-        hidden = sigma(value).T
+        value = hidden.dot(W.T) + b.T
+        hidden = sigma(value)
         values += [value.copy()]
     V, c = model[-1]
-    outputs = softmax(V.dot(hidden.T) + c)
-    values += [outputs.copy()]
+    val = hidden.dot(V.T) + c.T
+    outputs = softmax(val)
+    values += [val.copy()]
     return outputs, values
+
+
+def backprop_step(dEdY, W, gd, Y, X):
+    deltas = (gd(Y) * dEdY)
+    dEdX = deltas.dot(W)
+    dEdW = deltas.T.dot(X) / deltas.shape[0]
+    dEdb = deltas.mean(axis=0)
+    return dEdX, dEdW, dEdb
+
+
+def compute_loss(labels, outputs):
+    return (-labels * np.log(outputs)).sum(axis=1).mean()
 
 
 def backward_prop(data, labels, model, values, outputs):
@@ -103,37 +127,22 @@ def backward_prop(data, labels, model, values, outputs):
     :param model: Model as described in `train`
     :param values: Hidden variable values from forward propagation
     :param outputs: Outputs from forward propagation
-    :return: Loss and all derivatives
+    :return: All derivatives
     """
-    diffs = []
-    loss = (labels * outputs.T).sum()
-
     # matrix (batch_index, output_index)
-    err = -labels * (1. / outputs)
-
-    # tensor (batch, output, output2)
-    err = err * (outputs[:, :, None] - outputs[:, :, None]
-                 * (outputs[:, None, :]))
-    err = err.sum(axis=1)
-    c_diff = err.sum(axis=0)
-    v_diff = (err[:, :, None] * values[-1][:, None, :]).sum(axis=0)
-    diffs += [(v_diff, c_diff)]
-
-    err = (err[:, :, None] * model[-1][0][None, :, :]).sum(axis=1)
-
+    err = ((-labels * (1. / outputs))[:, :, None] *
+           softmax_grad(outputs)).sum(axis=1)
+    err, w_diff, b_diff = backprop_step(err, model[-1][0],
+                                        lambda x: np.ones_like(x),
+                                        values[-1], values[-2])
+    diffs = [(w_diff.copy(), b_diff.copy())]
+    values = [data] + values
     for i, (W, b) in enumerate(reversed(model[:-1])):
-        val = values[-i - 1]
-        if i + 2 <= len(values):
-            prev_val = values[-i - 2]
-        else:
-            prev_val = data
-        err = (err * val * (np.ones_like(val) - val))
-        w_diff = (err[:, :, None] * prev_val[:, None, :]).sum(axis=0)
-        b_diff = err.sum(axis=1)
-        diffs += [(w_diff, b_diff)]
-        err = (err[:, :, None] * W[None, :, :]).sum(axis=1)
+        err, w_diff, b_diff = backprop_step(err, W, sigma_grad,
+                                            values[-i-2], values[-i-3])
+        diffs += [(w_diff.copy(), b_diff.copy())]
 
-    return loss, reversed(diffs)
+    return [x for x in reversed(diffs)]
 
 
 def one_hot(data, out_size):

tests/test_mnist_mlp.py
@@ -1,8 +1,10 @@
 __author__ = 'dima'
 
 import numpy as np
+import numpy.testing
+import numpy.random
 
-from mnist_mlp import init_model, forward_prop, backward_prop
+from mnist_mlp import init_model, forward_prop, backward_prop, compute_loss
 
 
 def test_init_model():
@@ -21,8 +23,7 @@ def test_forward_prop():
     """Test forward propagation"""
     model = init_model(5, [3, 2], 10)
     output, values = forward_prop(np.array([[0, 0, 0, 0, 0]]), model)
-    assert output.shape == (10, 1)
-    print values
+    assert output.shape == (1, 10)
     assert len(values) == 3
 
 
@@ -34,7 +35,7 @@ def test_forward_prop_batch():
     batch = np.zeros((batch_size, inp_size))
     model = init_model(inp_size, [3, 2], out_size)
     output, values = forward_prop(batch, model)
-    assert output.shape == (out_size, batch_size)
+    assert output.shape == (batch_size, out_size)
     assert len(values) == 3
 
 
@@ -50,10 +51,7 @@ def test_backprop():
     label = np.zeros((batch_size, out_size))
     label[:, 0] = 1.
 
-    loss, diffs = backward_prop(batch, label, model, hiddens, output)
-    assert loss.shape == (batch_size,)
-    print loss
-    assert False
+    diffs = backward_prop(batch, label, model, hiddens, output)
 
 
 def test_backprop_batch():
@@ -69,4 +67,72 @@ def test_backprop_batch():
     label[:, 0] = 1.
 
     ans = backward_prop(batch, label, model, hiddens, output)
-    assert ans.shape == (batch_size,)
+    assert ans[0][0].shape == (batch_size,)
+
+
+def test_gradients_v():
+    """Test gradients for V"""
+    delta = 1e-5
+    batch_size = 4
+    inp_size = 5
+    out_size = 10
+    batch = np.random.normal(0, 0.1, (batch_size, inp_size))
+    model = init_model(inp_size, [3, 2], out_size)
+    output, hiddens = forward_prop(batch, model)
+
+    label = np.zeros((batch_size, out_size))
+    label[:, 0] = 1.
+
+    grads = backward_prop(batch, label, model, hiddens, output)
+
+    shape = model[-1][0].shape
+    num_grad = np.zeros(shape)
+    m, n = shape
+    for i in xrange(m):
+        for j in xrange(n):
+            outputs, _ = forward_prop(batch, model)
+            loss = compute_loss(label, outputs)
+
+            model[-1][0][i, j] += delta
+            outputs_plus, _ = forward_prop(batch, model)
+            loss_plus = compute_loss(label, outputs_plus)
+
+            model[-1][0][i, j] -= delta
+            num_grad[i, j] = (loss_plus - loss) / delta
+
+    print grads[-1][0], num_grad
+    np.testing.assert_allclose(grads[-1][0], num_grad, atol=1e-5)
+
+
+def test_gradients_c():
+    """Test gradients for c"""
+    delta = 1e-5
+    batch_size = 4
+    inp_size = 5
+    out_size = 10
+    batch = np.random.normal(0, 0.1, (batch_size, inp_size))
+    model = init_model(inp_size, [3, 2], out_size)
+    output, hiddens = forward_prop(batch, model)
+
+    label = np.zeros((batch_size, out_size))
+    label[:, 0] = 1.
+
+    grads = backward_prop(batch, label, model, hiddens, output)
+
+    V = model[-1][0]
+    diffs = np.zeros_like(V)
+    for i in xrange(V.shape[0]):
+        for j in xrange(V.shape[1]):
+            outputs1, _ = forward_prop(batch, model)
+            V[i, j] += delta
+            model[-1][0] = V
+            outputs2, _ = forward_prop(batch, model)
+            V[i, j] -= delta
+            model[-1][0] = V
+            diffs[i, j] = (outputs2 - outputs1) / delta
+
+    np.testing.assert_allclose(grads, diffs)
+
+
+if __name__ == '__main__':
+    test_backprop()