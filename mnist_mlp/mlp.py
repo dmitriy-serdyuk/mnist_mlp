1b4b2dc8c86c129dc42497ae0c3fc37b5800241a
dima <serdyuk.dmitriy@gmail.com>
2015-01-19 09:33:21 -0500

    Added docstirngs

--- mnist_mlp/mlp.py
+++ mnist_mlp/mlp.py
@@ -6,6 +6,17 @@ from utils import softmax, softmax_grad, sigma, sigma_grad, pairwise
 
 
 def init_model(num_feat, layer_sizes, num_out, rng=None, seed=1):
+    """
+    Randomly initializes a model
+
+    :param num_feat: Number of features
+    :param layer_sizes: An array of layer sizes
+    :param num_out: Number of outputs
+    :param rng: Random number generator
+    :param seed: Seed to create a random number generator if `rng` was not
+           passed
+    :return: Model
+    """
     model = []
     if rng is None:
         rng = np.random.RandomState(seed)
@@ -17,15 +28,15 @@ def init_model(num_feat, layer_sizes, num_out, rng=None, seed=1):
     return model
 
 
-def forward_prop(batch, model):
+def forward_prop(feat_batch, model):
     """
     Performs forward propagation and returns the last layer values
-    :param batch: Batch of data
+    :param feat_batch: Batch of data
     :param model: Model to use as described in `train`
     :return: List of outputs for each data point from `data` and values
              obtained
     """
-    hidden = batch
+    hidden = feat_batch
     values = []
     for W, b in model[:-1]:
         value = hidden.dot(W.T) + b.T
@@ -39,17 +50,27 @@ def forward_prop(batch, model):
 
 
 def backprop_step(dEdY, W, gd, Y, X):
-    deltas = (gd(Y) * dEdY)
+    """
+    Performs one step of backpropagation
+
+    :param dEdY: Previous gradient
+    :param W: Weight matrix for this iteration
+    :param gd: Gradient function with respect to `Y`
+    :param Y: Output of the layer
+    :param X: Input of the layer
+    :return: Gradient, gradient of `W`, gradient of bias `b`
+    """
+    deltas = gd(Y) * dEdY
     dEdX = deltas.dot(W)
     dEdW = deltas.T.dot(X) / deltas.shape[0]
     dEdb = deltas.mean(axis=0, keepdims=True).T
     return dEdX, dEdW, dEdb
 
 
-def backward_prop(data, labels, model, hiddens, outputs):
+def backward_prop(feats, labels, model, hiddens, outputs):
     """
     Performs backprop
-    :param data: Batch of data (batch_index, data_index)
+    :param feats: Batch of features (batch_index, data_index)
     :param labels: Labels for batch of data
     :param model: Model as described in `train`
     :param hiddens: Hidden variable values from forward propagation
@@ -63,7 +84,7 @@ def backward_prop(data, labels, model, hiddens, outputs):
                                         lambda x: np.ones_like(x),
                                         hiddens[-1], hiddens[-2])
     diffs = [(w_diff.copy(), b_diff.copy())]
-    hiddens = [data] + hiddens
+    hiddens = [feats] + hiddens
     for i, (W, b) in enumerate(reversed(model[:-1])):
         err, w_diff, b_diff = backprop_step(err, W, sigma_grad,
                                             hiddens[-i-2], hiddens[-i-3])
--- mnist_mlp/mnist_mlp.py
+++ mnist_mlp/mnist_mlp.py
@@ -11,34 +11,52 @@ from mlp import forward_prop, backward_prop, init_model
 
 
 def main(filename, out_size, batch_size, **kwargs):
+    """
+    Manages training loop and runs testing
+
+    :param filename: File containing dataset
+    :param out_size: Output dimensionality
+    :param batch_size: desired size of batch
+    :param kwargs: Arguments to be passed to the trainig loop
+    """
     train_data, valid_data, test_data = load_data(filename, out_size,
                                                   batch_size)
 
-    train_data, train_labels = train_data
-    valid_data, valid_labels = valid_data
-    test_data, test_labels = test_data
+    train_feats, train_labels = train_data
+    valid_feats, valid_labels = valid_data
+    test_feats, test_labels = test_data
 
     rng = np.random.RandomState(1)
-    model = train(train_data, train_labels, valid_data, valid_labels, rng=rng,
+    model = train(train_feats, train_labels, valid_feats, valid_labels, rng=rng,
                   **kwargs)
     with open('model.pkl', 'w') as fout:
         pkl.dump(model, fout)
 
-    loss, misclass = test(test_data, test_labels, model)
+    loss, misclass = test(test_feats, test_labels, model)
     print '.. testing error', loss, 'misclassification', misclass
 
 
 @timing
-def train(data, labels, valid_data, valid_labels, n_epochs,
+def train(feats, labels, valid_data, valid_labels, n_epochs,
           learn_rate, num_layers, n_hiddens, rng, **kwargs):
     """
-    Trains model
-    :param data: Training data
-    :param labels: Correct labels for `data`
+    Trains model, saves it into `model.pkl` every epoch.
+
+    :param feats: Training features
+    :param labels: Correct labels for `feats`
+    :param valid_data: Validation features
+    :param valid_labels: Validation labels
+    :param n_epochs: Number of epochs to train
+    :param learn_rate: Learning rate
+    :param num_layers: Number of layer of MLP
+    :param n_hiddens: Number of hidden units in each layer of MLP
+    :param rng: Random number generator
+    :param kwargs: Other parameters:
+           `model_file`: if passed, loads model from the file
     :return: Trained model, list of pairs (W, b) weight matrix W and bias b
              for each layer
     """
-    inp_size = data.shape[2]
+    inp_size = feats.shape[2]
     out_size = labels.shape[2]
     if rng is None:
         rng = np.random.RandomState(1)
@@ -54,14 +72,14 @@ def train(data, labels, valid_data, valid_labels, n_epochs,
     print '.. starting training'
     for epoch in xrange(n_epochs):
         train_loss = 0.
-        for batch, label in zip(data, labels):
+        for batch, label in zip(feats, labels):
             outputs, hiddens = forward_prop(batch, model)
             grads = backward_prop(batch, label, model, hiddens, outputs)
             train_loss += compute_loss(label, outputs)
             for (W, b), (grad_w, grad_b) in zip(model, grads):
                 W -= learn_rate * grad_w
                 b -= learn_rate * grad_b
-        train_loss /= data.shape[0]
+        train_loss /= feats.shape[0]
         valid_loss = 0.
         for batch, label in zip(valid_data, valid_labels):
             outputs, _ = forward_prop(batch, model)
@@ -76,22 +94,22 @@ def train(data, labels, valid_data, valid_labels, n_epochs,
     return model
 
 
-def test(data, labels, model, **kwargs):
+def test(feats, labels, model, **kwargs):
     """
     Computes testing error
-    :param data: Testing data
-    :param labels: Correct labels for `data`
+    :param feats: Testing features
+    :param labels: Correct labels for `feats`
     :param model: Trained model as described in function `train`
-    :return: testing error
+    :return: Testing loss, testing misclassification
     """
     test_loss = 0.
     test_misclass = 0.
-    for batch, label in zip(data, labels):
+    for batch, label in zip(feats, labels):
         outputs, _ = forward_prop(batch, model)
         test_loss += compute_loss(label, outputs)
         test_misclass += compute_misclass(label, outputs)
-    test_loss /= data.shape[0]
-    test_misclass /= data.shape[0] * data.shape[1]
+    test_loss /= feats.shape[0]
+    test_misclass /= feats.shape[0] * feats.shape[1]
     return test_loss, test_misclass
 
 
